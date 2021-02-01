99c644faa57257bc53eedda0ef81a7dfde61b33d
Kenia Sandoval <ksandoval-lopez21@basecampcodingacademy.org>
2021-02-01 11:11:44 -0600

    added chapter 10

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'accounts',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,4 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL= 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home' 
 LOGOUT_REDIRECT_URL = 'home'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
