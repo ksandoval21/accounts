f3b2389a2931b89d1ccabfda5c017ccb2ea57d7f
Kenia Sandoval <ksandoval-lopez21@basecampcodingacademy.org>
2021-02-08 14:24:15 -0600

    chapter 13 and most of chapter 14

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('pages.urls')), 
+    path('articles/', include('articles.urls')), # new
+    path('', include('pages.urls')),
 ]
+
